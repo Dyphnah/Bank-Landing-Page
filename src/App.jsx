392179a37c682a8606e4ed066bedee0cc8d60eea
Dyphnah <nyandukodyphnah4@gmail.com>
2025-05-09 05:03:23 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Navbar from './Navbar.jsx'
+
+function App() {
+  console.log("I'm working on this");
+
+  return (
+    <>
+      <Navbar/>
+
+      <h1>Coding a Landing Page. Yaay!</h1>
+     
+    </>
+  )
+}
+
+export default App
