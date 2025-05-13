ae12d5f85be8b84dad156b5fe91b2306825dd889
Dyphnah <nyandukodyphnah4@gmail.com>
2025-05-13 05:36:06 -0400

    responsive-design

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,41 +1,37 @@
-import React from "react";
+
+import { useState } from "react";
 import "./Navbar.css";
 import logo from './assets/images/logo.svg';
+import hamburgerIcon from './assets/images/icon-hamburger.svg'; 
 import { Link } from 'react-router-dom';
 
-
 function Navbar() {
-    
-    return (
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-        <nav className="nav-items">
-            <div className="logo">
-                <img src={logo} alt="bank_logo" />
-            </div>
-            <div className="nav-li">
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li> 
-                    <li>
-                        <Link to="/blog">Blog</Link>
-                    </li>
-                    <li>
-                        <Link to="/careers">Careers</Link>
-                    </li>
-                    <li className="last">
-                        <Link to="/request">Request Invite</Link>
-                    </li>
-                </ul>
-             </div>   
-            </nav>
-    )
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+  return (
+    <nav className="nav-items">
+      <div className="logo">
+        <img src={logo} alt="bank_logo" />
+      </div>
+
+      <div className="hamburger-icon" onClick={toggleMenu}>
+        <img src={hamburgerIcon} alt="hamburger menu" />
+      </div>
+
+      <div className={`nav-li ${isMenuOpen ? 'open' : ''}`}>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/careers">Careers</Link></li>
+          <li className="last"><Link to="/request">Request Invite</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
